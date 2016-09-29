73f4d791a7d65a30949d4f82f2c1c37e36eb7b40
3050292018@qq.com <3050292018@qq.com>
2016-09-29 18:41:05 +0800

    待发布版本

---
+++ gen/com/gizwits/opensource/gokit/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.gizwits.opensource.gokit;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/gizwits/opensource/gokit/ControlModule/GosControlModuleBaseActivity.java
@@ -0,0 +1,116 @@
+package com.gizwits.opensource.gokit.ControlModule;
+
+import com.gizwits.gizwifisdk.api.GizWifiDevice;
+import com.gizwits.gizwifisdk.enumration.GizWifiDeviceNetStatus;
+import com.gizwits.gizwifisdk.enumration.GizWifiErrorCode;
+import com.gizwits.gizwifisdk.listener.GizWifiDeviceListener;
+import com.gizwits.opensource.gokit.CommonModule.GosBaseActivity;
+
+import android.view.MenuItem;
+
+public class GosControlModuleBaseActivity extends GosBaseActivity {
+
+	GizWifiDeviceListener gizWifiDeviceListener = new GizWifiDeviceListener() {
+
+		/** 用于设备订阅 */
+		public void didSetSubscribe(GizWifiErrorCode result, GizWifiDevice device, boolean isSubscribed) {
+			GosControlModuleBaseActivity.this.didSetSubscribe(result, device, isSubscribed);
+		};
+
+		/** 用于设备状态 */
+		public void didReceiveData(GizWifiErrorCode result, GizWifiDevice device,
+				java.util.concurrent.ConcurrentHashMap<String, Object> dataMap, int sn) {
+			GosControlModuleBaseActivity.this.didReceiveData(result, device, dataMap, sn);
+		};
+
+		/** 用于设备硬件信息 */
+		public void didGetHardwareInfo(GizWifiErrorCode result, GizWifiDevice device,
+				java.util.concurrent.ConcurrentHashMap<String, String> hardwareInfo) {
+			GosControlModuleBaseActivity.this.didGetHardwareInfo(result, device, hardwareInfo);
+		};
+
+		/** 用于修改设备信息 */
+		public void didSetCustomInfo(GizWifiErrorCode result, GizWifiDevice device) {
+			GosControlModuleBaseActivity.this.didSetCustomInfo(result, device);
+		};
+
+		/** 用于设备状态变化 */
+		public void didUpdateNetStatus(GizWifiDevice device, GizWifiDeviceNetStatus netStatus) {
+			GosControlModuleBaseActivity.this.didUpdateNetStatus(device, netStatus);
+		};
+
+	};
+
+	/**
+	 * 设备订阅回调
+	 * 
+	 * @param result
+	 *            错误码
+	 * @param device
+	 *            被订阅设备
+	 * @param isSubscribed
+	 *            订阅状态
+	 */
+	protected void didSetSubscribe(GizWifiErrorCode result, GizWifiDevice device, boolean isSubscribed) {
+	}
+
+	/**
+	 * 设备状态回调
+	 * 
+	 * @param result
+	 *            错误码
+	 * @param device
+	 *            当前设备
+	 * @param dataMap
+	 *            当前设备状态
+	 * @param sn
+	 *            命令序号
+	 */
+	protected void didReceiveData(GizWifiErrorCode result, GizWifiDevice device,
+			java.util.concurrent.ConcurrentHashMap<String, Object> dataMap, int sn) {
+	}
+
+	/**
+	 * 设备硬件信息回调
+	 * 
+	 * @param result
+	 *            错误码
+	 * @param device
+	 *            当前设备
+	 * @param hardwareInfo
+	 *            当前设备硬件信息
+	 */
+	protected void didGetHardwareInfo(GizWifiErrorCode result, GizWifiDevice device,
+			java.util.concurrent.ConcurrentHashMap<String, String> hardwareInfo) {
+	}
+
+	/**
+	 * 修改设备信息回调
+	 * 
+	 * @param result
+	 *            错误码
+	 * @param device
+	 *            当前设备
+	 */
+	protected void didSetCustomInfo(GizWifiErrorCode result, GizWifiDevice device) {
+	}
+
+	/**
+	 * 设备状态变化回调
+	 * 
+	 * @param arg0
+	 * @param arg1
+	 */
+	protected void didUpdateNetStatus(GizWifiDevice device, GizWifiDeviceNetStatus netStatus) {
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			this.finish();
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
